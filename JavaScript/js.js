921adda940915b258285c797f82e5c82343efcd8
Gabriel Felipe <gabrielfelipe11102002@gmail.com>
2023-02-06 19:38:22 -0300

    add

--- JavaScript/js.js
+++ JavaScript/js.js
@@ -1,10 +1,13 @@
 function menuShow() {
     let menuMobile = document.querySelector('.pai-mobile-menu');
+    
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
         document.querySelector('.icon').src = "../Imagens/menu_white_36dp.svg";
+        
     } else {
         menuMobile.classList.add('open');
         document.querySelector('.icon').src = "../Imagens/close_white_36dp.svg";
+        
     }
 }
